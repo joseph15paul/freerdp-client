1c408a74069bae1996ebc60b4e4e24f4e88ef634
user <user@ubuntu2004template.(none)>
2025-06-19 09:16:38 -0400

    added client

---
+++ client.cxx
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <freerdp/freerdp.h>
+#include <freerdp/client/channels.h>
+
+int main(int argc, char * argv[]){
+
+    std::cout << "hello world";
+    freerdp* instance = freerdp_new();
+    return 1;
+}
